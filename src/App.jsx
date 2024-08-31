0584ae991d3858408e618fbcf0f14834005bfe38
aojha@Shyam <aojha@gmail.com>
2024-08-31 13:43:07 +0530

    Project finished. using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import ButtonsContainer from "./Components/ButtonsContainer";
 import Display from "./Components/Display";
 
 function App() {
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
   return (
     <>
       <div className={styles.colculatorContainer}>
-        <Display></Display>
-        <ButtonsContainer />
+        <Display displayValue={calVal}></Display>
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
       </div>
     </>
   );
